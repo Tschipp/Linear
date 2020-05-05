7ce5158bf3588ffdf2dde8c73f0a2aa0a55dbe59
Tschipp <mrtschipp@gmail.com>
2020-05-05 13:33:41 +0200

    Added CI with Jenkins

--- src/main/java/tschipp/linear/Linear.java
+++ src/main/java/tschipp/linear/Linear.java
@@ -2,24 +2,23 @@ package tschipp.linear;
 
 import java.io.File;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import tschipp.linear.common.CommonProxy;
 
 @EventBusSubscriber
-@Mod(modid = Linear.MODID, name = Linear.NAME, version = Linear.VERSION, guiFactory = "tschipp.linear.client.gui.GuiFactoryLinear", dependencies = Linear.DEPENDENCIES, acceptedMinecraftVersions = Linear.ACCEPTED_VERSIONS)
+@Mod(modid = Linear.MODID, name = Linear.NAME, version = Linear.VERSION, guiFactory = "tschipp.linear.client.gui.GuiFactoryLinear", dependencies = Linear.DEPENDENCIES, acceptedMinecraftVersions = Linear.ACCEPTED_VERSIONS, certificateFingerprint =  Linear.CERTIFICATE_FINGERPRINT)
 public class Linear {
 
 	@SidedProxy(clientSide = "tschipp.linear.client.ClientProxy", serverSide = "tschipp.linear.common.CommonProxy")
@@ -30,13 +29,16 @@ public class Linear {
 	public static Linear instance;
 
 	public static final String MODID = "linear";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "GRADLE:VERSION";
 	public static final String NAME = "Linear";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2,1.13)";
 	public static final Logger LOGGER = LogManager.getFormatterLogger("Linear");
 	public static final String DEPENDENCIES = "";
 	public static File CONFIGURATION_FILE;
- 
+	public static final String CERTIFICATE_FINGERPRINT = "fd21553434f4905f2f73ea7838147ac4ea07bd88";
+
+	public static boolean FINGERPRINT_VIOLATED = false;
+	
 	public static SimpleNetworkWrapper network;
 
 	@EventHandler
@@ -54,4 +56,11 @@ public class Linear {
 		Linear.proxy.postInit(event);
 	}
 
+	@EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+        
+		LOGGER.error("WARNING! Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with! If you didn't download the file from https://minecraft.curseforge.com/projects/linear or through any kind of mod launcher, immediately delete the file and re-download it from https://minecraft.curseforge.com/projects/linear");
+		FINGERPRINT_VIOLATED = true;
+	}
+	
 }
